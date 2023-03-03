f086594fd972fbbee9b7596ed05154d80013f03b
Milovan99 <milovanjakovljevic78@gmail.com>
2023-03-03 13:06:53 +0100

    Dice Roller

--- app/src/main/java/com/milovanjakovljevic/compose/MainActivity.kt
+++ app/src/main/java/com/milovanjakovljevic/compose/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.milovanjakovljevic.compose.composebasic.BusinessCard
+import com.milovanjakovljevic.compose.dice_roller.DiceRollerApp
+import com.milovanjakovljevic.compose.dice_roller.DiceWithAnimationAndButton
 import com.milovanjakovljevic.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,19 +21,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+               /* Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color(0xFF073042)
-                ) {
+                ) {*/
+                    //compose basic elements
                   // Article(message = getString(R.string.headline), paragraph =  getString(R.string.paragraph1), paragraph2 =  getString(R.string.paragraph2))
                   // TaskManager()
                   // ComposeQuadrantApp()
-                    BusinessCard()
+                  //  BusinessCard()
+
+                DiceRollerApp()
                 }
             }
         }
     }
-}
+
+
 
 
 
